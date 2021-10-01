28aa1f9dd80df209f4e76fb39590a7be1f8c7779
Sergio David Paez Suarez <spaezsuarez@gmail.com>
2021-09-30 22:49:28 -0500

    endpoint de prueba para hashear contraseñas

--- src/controller/clientController.py
+++ src/controller/clientController.py
@@ -1,12 +1,9 @@
 from fastapi import APIRouter, Depends
-
 from sqlalchemy.orm import Session
-
 from service.ClientService import get_clients_service, get_client_service, create_client_service, delete_client_service
-
 from config.databaseConfig import get_db
-
 from dto.Client import ClientRequest, ClientResponse
+from security.authSecurity import crypt_password
 
 clientController = APIRouter(prefix='/client')
 
@@ -14,11 +11,15 @@ clientController = APIRouter(prefix='/client')
 def get_all_clients(session: Session = Depends(get_db)):
     return get_clients_service(session)
 
+@clientController.get('/test/hash/{password}')
+def test(password):
+    return crypt_password(password)
+
 @clientController.get('/{id}', response_model=ClientResponse)
 def get_client(id, session: Session = Depends(get_db)):
     return get_client_service(session, id)
 
-@clientController.post('/save', response_model=ClientResponse)
+@clientController.post('/', response_model=ClientResponse)
 def create_client(client: ClientRequest, session: Session = Depends(get_db)):
     return create_client_service(session, client)
 
