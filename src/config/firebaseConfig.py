38b39950e9ebded6eb50df34f0daffeef2debc49
Jesús Bermúdez <jema0622@outlook.com>
2021-10-09 21:30:17 -0500

    Fixed docker strcuture

--- src/config/firebaseConfig.py
+++ src/config/firebaseConfig.py
@@ -3,7 +3,7 @@ from os import getenv
 from firebase_admin import credentials, initialize_app, storage
 
 def init_firebase():
-    _ecommerce_credentials = credentials.Certificate('../private/secret.json')
+    _ecommerce_credentials = credentials.Certificate('private/secret.json')
     initialize_app(_ecommerce_credentials, {
         'storageBucket': getenv('FIREBASE_BUCKET')
     })
